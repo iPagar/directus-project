ec45e449cf5d3d84fbb8a672978c11409c2732ce
Pavel <ipagar@icloud.com>
2021-12-15 15:25:58 +0300

    patches

--- green-frame-cms/extensions/endpoints/build-and-deploy/index.js
+++ green-frame-cms/extensions/endpoints/build-and-deploy/index.js
@@ -3,7 +3,7 @@ const http = require("http");
 module.exports = function registerEndpoint(router) {
   router.get("/deploy-production", async (req, res) => {
     http
-      .get("http://gatsby-prod:3000/", (resp) => {
+      .get("http://gatsby-prod:3000/", (data) => {
         // A chunk of data has been received.
         resp.on("data", (chunk) => {
           data += chunk;
